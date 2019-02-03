d78924b2efa4d3ff956689d09c3d696d6bf233db
entreaccolades <entreaccolades@gmail.com>
2019-02-03 18:43:21 +0100

    type: Component: desc: creation d'un component de démonstration Homepage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+/*
+  Notre 1er composant Homepage.
+  Le nom du composant doit toujous être écrit avec la première lettre en majuscule.
+  Si ce n'est le cas, React interprète ce composant comme une balise html et sera restituée en tant que telle dans le DOM.
+  Une erreur sera soulevée dans la console sans toutefois bloquer l'application.
+  Illustration de l'erreur avec un composant déclaré en homepage au lieu de Homepage : 
+  index.js:1446 Warning: The tag <homepage> is unrecognized in this browser. If you meant to render a React component, start its name with an uppercase letter.
+    in homepage (at App.js:50)
+    in div (at App.js:35)
+    in App (at src/index.js:7)
+*/
+
+const Homepage = () => (  //Syntax ES6 avec arrow function
+    <h2>Je suis le composant Homepage</h2>
+  )
+;
+
+/* ES5 syntax :
+
+function Homepage (){
+  return  <main><h2>Je suis le composant Homepa</h2></main>
+}
+
+*/
+
 class App extends Component {
   render() {
+  
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +49,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+{/* Notre Homepage composant insérer ici */}
+        <Homepage />
       </div>
     );
   }
