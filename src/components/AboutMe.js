c228e406726be98454de3662fa6d89cde49ce760
entreaccolades <entreaccolades@gmail.com>
2019-02-09 20:44:05 +0100

    type: component desc: création components de démonstration. simple renvoi de jsx sans props ni state

---
+++ src/components/AboutMe.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const AboutMe = () => (
+  <section>
+    <h1>
+      A mon propos
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, eaque
+        harum a distinctio incidunt quam commodi quod voluptatem eos recusandae
+        necessitatibus voluptatibus alias quas consequuntur quasi laudantium
+        laboriosam. Eaque, culpa?
+      </p>
+    </h1>
+  </section>
+); 
+export default AboutMe;
+
---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Homepage = () => (
+
+    <section>
+        <h1>homepage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam itaque, debitis ipsam reprehenderit vel nisi consectetur praesentium aut cupiditate ea nihil magni commodi suscipit, accusamus aspernatur dolores ipsum consequatur.</p>
+        <p>Maxime labore laudantium exercitationem ea id aspernatur corporis quidem dicta reprehenderit soluta minus architecto beatae, asperiores excepturi a aliquid quasi cum repellendus temporibus? Earum qui, at harum obcaecati dolores nemo?</p>
+        <p>Magnam voluptatum, velit dolor commodi esse doloribus recusandae quam atque nisi earum, cumque minima? Architecto maxime, animi ea quibusdam odit natus officia eveniet possimus expedita voluptates sunt vero, recusandae sit!</p>
+    </section>
+);
+
+export default Homepage;
