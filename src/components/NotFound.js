7d896edb3ca12c1e3b6455c758ff48c632fcbac5
entreaccolades <entreaccolades@gmail.com>
2019-02-11 09:23:46 +0100

    type: component desc: creation <component /> => NotFound / route incorrecte

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => (
+  <section>
+    <h1>Contenu non trouvé</h1>
+    <p>
+      Cela arrive ! Voyons comment nous pouvons vous aider à reprendre là où
+      vous en étiez.
+    </p>
+  </section>
+);
+export default NotFound;
