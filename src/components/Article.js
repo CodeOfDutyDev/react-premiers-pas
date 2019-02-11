aa1b531cd003b6412bbc1741f3507f0f0fdd3a14
entreaccolades <entreaccolades@gmail.com>
2019-02-11 09:23:08 +0100

    type: component desc: creation <component /> => Article

---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Article = () => (
+  <section>
+    <h1>Je suis l'article </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, repellendus
+      amet pariatur voluptatum reprehenderit consectetur deleniti! Magni rem
+      nostrum modi consequatur maiores, ratione laudantium repudiandae ad, vitae
+      autem saepe! Eligendi.
+    </p>
+  </section>
+);
+export default Article;
