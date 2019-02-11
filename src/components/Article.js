eb37e5c4ceeeeeae4e31945ab0eafab93626a6d1
EntreAccolades <entreaccolades@gmail.com>
2019-02-11 18:46:29 +0100

    type: component desc: modification pour affichage article avec props + fn filter.map sur l'id passé en props

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,20 @@
 import React from "react";
+import article from "../mocks/article.json";
 
-const Article = () => (
-  <section>
-    <h1>Je suis l'article </h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, repellendus
-      amet pariatur voluptatum reprehenderit consectetur deleniti! Magni rem
-      nostrum modi consequatur maiores, ratione laudantium repudiandae ad, vitae
-      autem saepe! Eligendi.
-    </p>
-  </section>
-);
+const Article = ({match:{params:{id} }}) => {
+
+	return (
+		<section>
+			{article.filter(el => el.id === parseInt(id)).map((el, k) => {
+      
+				return (
+					<div key={k}>
+						<h1>{el.title}</h1>
+						<p>{el.content}</p>
+					</div>
+				);
+			})}
+		</section>
+	);
+};
 export default Article;
