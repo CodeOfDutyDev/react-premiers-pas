889ffeeb5f046dd12416a497d8498d08d001c271
entreaccolades <entreaccolades@gmail.com>
2019-02-12 16:45:49 +0100

    type: comments

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,21 @@
 import React from "react";
 import article from "../mocks/article.json";
 
+/*
+
+Component origin : Article
+Url cible : /article/:id
+
+Desc: 
+Affiche l'article par son id passé en params d'url
+
+Spec: 
+id transmis est de type <string> tandis que le id de l'objet article est de type <number>.
+utilisation de parseInt pour passé la props en type number.
+*/
+
 const Article = props => (
+	
   <section>
     {article
       .filter(el => el.id === parseInt(props.match.params.id))
