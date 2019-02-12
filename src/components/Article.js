4faa3ec03c55a3ff71dcbc8bc044810eab392f1b
entreaccolades <entreaccolades@gmail.com>
2019-02-12 16:32:56 +0100

    type: component desc: modification render component et props

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,18 +1,18 @@
 import React from "react";
 import article from "../mocks/article.json";
 
-const Article = (props) => (
-	
-		<section>
-			{ article.filter(el => el.id === parseInt(props.match.params.id)).map((el, k) => {
-      
-				return (
-					<div key={k}>
-						<h1>{el.title}</h1>
-						<p>{el.content}</p>
-					</div>
-				);
-			})}
-		</section>
+const Article = props => (
+  <section>
+    {article
+      .filter(el => el.id === parseInt(props.match.params.id))
+      .map((el, k) => {
+        return (
+          <div key={k}>
+            <h1>{el.title}</h1>
+            <p>{el.content}</p>
+          </div>
+        );
+      })}
+  </section>
 );
 export default Article;
